806708c592d5f6bc5e000b1ec1b7117caa2a7c83
Gurwinder Singh <vargwin@gmail.com>
2020-02-08 00:22:16 +0530

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub enum JsmnType {
+pub enum TokenKind {
     Undefined,
     Object,
     Array,
@@ -7,7 +7,7 @@ pub enum JsmnType {
     Primitive,
 }
 
-impl Default for JsmnType {
+impl Default for TokenKind {
     fn default() -> Self {
         Self::Undefined
     }
@@ -15,7 +15,7 @@ impl Default for JsmnType {
 
 #[derive(Debug, Copy, Clone)]
 #[repr(i32)]
-pub enum JsmnError {
+pub enum Error {
     /// The string is not a full JSON packet, more bytes expected
     Part,
     /// Invalid character inside JSON string
@@ -26,281 +26,255 @@ pub enum JsmnError {
 
 #[derive(Default, Debug, Copy, Clone)]
 #[repr(C)]
-pub struct JsmnToken {
-    pub kind: JsmnType,
+pub struct Token {
+    pub kind: TokenKind,
     pub start: i32,
     pub end: i32,
     pub size: i32,
 }
 
 #[derive(Default)]
-pub struct JsmnParser {
+pub struct JsonParser {
     pub pos: u32,
     pub tok_next: u32,
     pub tok_super: i32,
 }
 
-impl JsmnParser {
+impl JsonParser {
     pub fn new() -> Self {
         Self {
             tok_super: -1,
             ..Default::default()
         }
     }
-}
-/* *
- * Allocates a fresh unused token from the token pool.
- */
-fn jsmn_alloc_token(mut parser: &mut JsmnParser, tokens: &mut [JsmnToken]) -> Option<usize> {
-    if parser.tok_next as usize >= tokens.len() {
-        return None;
+
+    ///
+    /// Run JSON parser. It parses a JSON data string into and array of tokens, each
+    /// describing a single JSON object.
+    ///
+    /// Parse JSON string and fill tokens.
+    pub fn parse(&mut self, js: &[u8], tokens: &mut [Token]) -> Result<i32, Error> {
+        let mut count = self.tok_next as i32;
+        while (self.pos as usize) < js.len() {
+            let c = js[self.pos as usize];
+            match c {
+                b'{' | b'[' => {
+                    count += 1;
+                    let i = self.alloc_token(tokens).ok_or(Error::NoMemory)?;
+                    if self.tok_super != -1 {
+                        let mut t = &mut tokens[self.tok_super as usize];
+                        t.size += 1
+                    }
+                    let token = &mut tokens[i];
+                    token.kind = if c == b'{' {
+                        TokenKind::Object
+                    } else {
+                        TokenKind::Array
+                    };
+                    token.start = self.pos as i32;
+                    self.tok_super = self.tok_next as i32 - 1;
+                }
+                b'}' | b']' => {
+                    let type_0 = if c == b'}' {
+                        TokenKind::Object
+                    } else {
+                        TokenKind::Array
+                    };
+                    let mut i = (self.tok_next - 1) as i32;
+                    while i >= 0 {
+                        let token = &mut tokens[i as usize];
+                        if token.start != -1 && token.end == -1 {
+                            if token.kind as u32 != type_0 as u32 {
+                                return Err(Error::Invalid);
+                            }
+                            self.tok_super = -1;
+                            token.end = self.pos as i32 + 1;
+                            break;
+                        } else {
+                            i -= 1
+                        }
+                    }
+                    // Error if unmatched closing bracket
+                    if i == -1 {
+                        return Err(Error::Invalid);
+                    }
+                    while i >= 0 {
+                        let token = &mut tokens[i as usize];
+                        if token.start != -1 && token.end == -1 {
+                            self.tok_super = i;
+                            break;
+                        } else {
+                            i -= 1
+                        }
+                    }
+                }
+                b'"' => {
+                    let r = self.jsmn_parse_string(js, tokens)?;
+                    if r < 0 {
+                        return Ok(r);
+                    }
+                    count += 1;
+                    if self.tok_super != -1 {
+                        tokens[self.tok_super as usize].size += 1
+                    }
+                }
+                b'\t' | b'\r' | b'\n' | b' ' => {}
+                b':' => self.tok_super = self.tok_next as i32 - 1,
+                b',' => {
+                    if self.tok_super != -1
+                        && tokens[self.tok_super as usize].kind != TokenKind::Array
+                        && tokens[self.tok_super as usize].kind != TokenKind::Object
+                    {
+                        let mut i = self.tok_next as i32 - 1;
+                        while i >= 0 {
+                            let t = &tokens[i as usize];
+                            if let TokenKind::Array | TokenKind::Object = t.kind {
+                                if t.start != -1 && t.end == -1 {
+                                    self.tok_super = i;
+                                    break;
+                                }
+                            }
+                            i -= 1
+                        }
+                    }
+                }
+                _ => {
+                    // In non-strict mode every unquoted value is a primitive
+                    let r = self.jsmn_parse_primitive(js, tokens)?;
+                    if r < 0 {
+                        return Ok(r);
+                    }
+                    count += 1;
+                    if self.tok_super != -1 {
+                        tokens[self.tok_super as usize].size += 1
+                    }
+                }
+            }
+            self.pos += 1;
+        }
+        let mut i = self.tok_next as i32 - 1;
+        while i >= 0 {
+            // Unmatched opened object or array
+            if tokens[i as usize].start != -1 && tokens[i as usize].end == -1 {
+                return Err(Error::Part);
+            }
+            i -= 1
+        }
+        Ok(count)
     }
-    let idx = parser.tok_next as usize;
-    parser.tok_next += 1;
-    let tok = &mut tokens[idx];
-    tok.end = -1;
-    tok.start = tok.end;
-    tok.size = 0;
-    Some(idx)
+
+    /// Fills next available token with JSON primitive.
+    fn jsmn_parse_primitive(&mut self, js: &[u8], tokens: &mut [Token]) -> Result<i32, Error> {
+        let start = self.pos as i32;
+        while (self.pos as usize) < js.len() {
+            match js[self.pos as usize] {
+                b':' | b'\t' | b'\r' | b'\n' | b' ' | b',' | b']' | b'}' => break,
+                _ => {}
+            }
+
+            if (js[self.pos as usize]) < 32 || js[self.pos as usize] >= 127 {
+                self.pos = start as u32;
+                return Err(Error::Invalid);
+            }
+            self.pos += 1;
+        }
+
+        match self.alloc_token(tokens) {
+            Some(i) => {
+                fill_token(&mut tokens[i], TokenKind::Primitive, start, self.pos as i32);
+            }
+            None => {
+                self.pos = start as u32;
+                return Err(Error::NoMemory);
+            }
+        }
+
+        self.pos -= 1;
+        Ok(0)
+    }
+
+    /// Fills next token with JSON string.
+    fn jsmn_parse_string(&mut self, js: &[u8], tokens: &mut [Token]) -> Result<i32, Error> {
+        let start: i32 = self.pos as i32;
+        self.pos += 1;
+        // Skip starting quote
+        while (self.pos as usize) < js.len() {
+            let c = js[self.pos as usize];
+            // Quote: end of string
+            if c == b'\"' {
+                let token = self.alloc_token(tokens);
+                if token.is_none() {
+                    self.pos = start as u32;
+                    return Err(Error::NoMemory);
+                }
+                match token {
+                    Some(i) => fill_token(&mut tokens[i], TokenKind::Str, start + 1, self.pos as _),
+                    None => {
+                        self.pos = start as _;
+                        return Err(Error::NoMemory);
+                    }
+                };
+                return Ok(0);
+            }
+            // Backslash: Quoted symbol expected
+            if c as i32 == '\\' as i32 && (self.pos as usize + 1) < js.len() {
+                self.pos += 1;
+                match js[self.pos as usize] {
+                    b'"' | b'/' | b'\\' | b'b' | b'f' | b'r' | b'n' | b't' => {}
+                    b'u' => {
+                        // Allows escaped symbol \uXXXX
+                        self.pos += 1;
+                        let mut i = 0;
+                        while i < 4 && (self.pos as usize) < js.len() {
+                            // If it isn't a hex character we have an error
+
+                            let is_hex = match js[self.pos as usize] {
+                                _c @ b'0'..=b'9' => true,
+                                _c @ b'A'..=b'F' => true,
+                                _c @ b'a'..=b'f' => true,
+                                _ => false,
+                            };
+                            if !is_hex {
+                                self.pos = start as u32;
+                                return Err(Error::Invalid);
+                            }
+                            self.pos += 1;
+                            i += 1
+                        }
+                        self.pos -= 1;
+                    }
+                    _ => {
+                        /* Unexpected symbol */
+                        self.pos = start as u32;
+                        return Err(Error::Invalid);
+                    }
+                }
+            }
+            self.pos += 1;
+        }
+        self.pos = start as u32;
+        Err(Error::Part)
+    }
+
+    /// Allocates a fresh unused token from the token pool.
+    fn alloc_token(&mut self, tokens: &mut [Token]) -> Option<usize> {
+        if self.tok_next as usize >= tokens.len() {
+            return None;
+        }
+        let idx = self.tok_next as usize;
+        self.tok_next += 1;
+        let tok = &mut tokens[idx];
+        tok.end = -1;
+        tok.start = tok.end;
+        tok.size = 0;
+        Some(idx)
+    }
 }
-/* *
- * Fills token type and boundaries.
- */
-fn jsmn_fill_token(token: &mut JsmnToken, type_0: JsmnType, start: i32, end: i32) {
+
+/// Fills token type and boundaries.
+fn fill_token(token: &mut Token, type_0: TokenKind, start: i32, end: i32) {
     token.kind = type_0;
     token.start = start;
     token.end = end;
     token.size = 0;
 }
-/* *
- * Fills next available token with JSON primitive.
- */
-fn jsmn_parse_primitive(
-    parser: &mut JsmnParser,
-    js: &[u8],
-    tokens: &mut [JsmnToken],
-) -> Result<i32, JsmnError> {
-    let start = parser.pos as i32;
-    while (parser.pos as usize) < js.len() {
-        match js[parser.pos as usize] {
-            b':' | b'\t' | b'\r' | b'\n' | b' ' | b',' | b']' | b'}' => break,
-            _ => {}
-        }
-
-        if (js[parser.pos as usize]) < 32 || js[parser.pos as usize] >= 127 {
-            parser.pos = start as u32;
-            return Err(JsmnError::Invalid);
-        }
-        parser.pos += 1;
-    }
-
-    let token = jsmn_alloc_token(parser, tokens);
-    if token.is_none() {
-        parser.pos = start as u32;
-        return Err(JsmnError::NoMemory);
-    }
-    jsmn_fill_token(
-        &mut tokens[token.unwrap()],
-        JsmnType::Primitive,
-        start,
-        parser.pos as i32,
-    );
-    parser.pos -= 1;
-    Ok(0)
-}
-/* *
- * Fills next token with JSON string.
- */
-fn jsmn_parse_string(
-    mut parser: &mut JsmnParser,
-    js: &[u8],
-    tokens: &mut [JsmnToken],
-) -> Result<i32, JsmnError> {
-    let start: i32 = parser.pos as i32;
-    parser.pos += 1;
-    /* Skip starting quote */
-    while (parser.pos as usize) < js.len() {
-        let c = js[parser.pos as usize];
-        /* Quote: end of string */
-        if c == b'\"' {
-            let token = jsmn_alloc_token(parser, tokens);
-            if token.is_none() {
-                parser.pos = start as u32;
-                return Err(JsmnError::NoMemory);
-            }
-            match token {
-                Some(i) => {
-                    jsmn_fill_token(&mut tokens[i], JsmnType::Str, start + 1, parser.pos as _)
-                }
-                None => {
-                    parser.pos = start as _;
-                    return Err(JsmnError::NoMemory);
-                }
-            };
-            return Ok(0);
-        }
-        /* Backslash: Quoted symbol expected */
-        if c as i32 == '\\' as i32 && (parser.pos as usize + 1) < js.len() {
-            parser.pos += 1;
-            match js[parser.pos as usize] {
-                b'"' | b'/' | b'\\' | b'b' | b'f' | b'r' | b'n' | b't' => {}
-                b'u' => {
-                    /* Allows escaped symbol \uXXXX */
-                    parser.pos += 1;
-                    let mut i = 0;
-                    while i < 4 && (parser.pos as usize) < js.len() {
-                        /* If it isn't a hex character we have an error */
-
-                        let is_hex = match js[parser.pos as usize] {
-                            _c @ b'0'..=b'9' => true,
-                            _c @ b'A'..=b'F' => true,
-                            _c @ b'a'..=b'f' => true,
-                            _ => false,
-                        };
-                        if !is_hex {
-                            parser.pos = start as u32;
-                            return Err(JsmnError::Invalid);
-                        }
-                        parser.pos += 1;
-                        i += 1
-                    }
-                    parser.pos -= 1;
-                }
-                _ => {
-                    /* Unexpected symbol */
-                    parser.pos = start as u32;
-                    return Err(JsmnError::Invalid);
-                }
-            }
-        }
-        parser.pos += 1;
-    }
-    parser.pos = start as u32;
-    Err(JsmnError::Part)
-}
-/* *
- * Run JSON parser. It parses a JSON data string into and array of tokens, each
- * describing
- * a single JSON object.
- */
-/* *
- * Parse JSON string and fill tokens.
- */
-pub fn jsmn_parse(
-    mut parser: &mut JsmnParser,
-    js: &[u8],
-    tokens: &mut [JsmnToken],
-) -> Result<i32, JsmnError> {
-    let mut count = parser.tok_next as i32;
-    while (parser.pos as usize) < js.len() {
-        let c = js[parser.pos as usize];
-        match c {
-            b'{' | b'[' => {
-                count += 1;
-                let token = jsmn_alloc_token(parser, tokens);
-                if token.is_none() {
-                    return Err(JsmnError::NoMemory);
-                }
-                if parser.tok_super != -1 {
-                    let mut t = &mut tokens[parser.tok_super as usize];
-                    t.size += 1
-                }
-                let token = &mut tokens[token.unwrap()];
-                token.kind = if c == b'{' {
-                    JsmnType::Object
-                } else {
-                    JsmnType::Array
-                };
-                token.start = parser.pos as i32;
-                parser.tok_super = parser.tok_next as i32 - 1;
-            }
-            b'}' | b']' => {
-                let type_0 = if c == b'}' {
-                    JsmnType::Object
-                } else {
-                    JsmnType::Array
-                };
-                let mut i = (parser.tok_next - 1) as i32;
-                while i >= 0 {
-                    let token = &mut tokens[i as usize];
-                    if token.start != -1 && token.end == -1 {
-                        if token.kind as u32 != type_0 as u32 {
-                            return Err(JsmnError::Invalid);
-                        }
-                        parser.tok_super = -1;
-                        token.end = parser.pos as i32 + 1;
-                        break;
-                    } else {
-                        i -= 1
-                    }
-                }
-                /* Error if unmatched closing bracket */
-                if i == -1 {
-                    return Err(JsmnError::Invalid);
-                }
-                while i >= 0 {
-                    let token = &mut tokens[i as usize];
-                    if token.start != -1 && token.end == -1 {
-                        parser.tok_super = i;
-                        break;
-                    } else {
-                        i -= 1
-                    }
-                }
-            }
-            b'"' => {
-                let r = jsmn_parse_string(parser, js, tokens)?;
-                if r < 0 {
-                    return Ok(r);
-                }
-                count += 1;
-                if parser.tok_super != -1 {
-                    tokens[parser.tok_super as usize].size += 1
-                }
-            }
-            b'\t' | b'\r' | b'\n' | b' ' => {}
-            b':' => parser.tok_super = parser.tok_next as i32 - 1,
-            b',' => {
-                if parser.tok_super != -1
-                    && tokens[parser.tok_super as usize].kind != JsmnType::Array
-                    && tokens[parser.tok_super as usize].kind != JsmnType::Object
-                {
-                    let mut i = parser.tok_next as i32 - 1;
-                    while i >= 0 {
-                        let t = &tokens[i as usize];
-                        if let JsmnType::Array | JsmnType::Object = t.kind {
-                            if t.start != -1 && t.end == -1 {
-                                parser.tok_super = i;
-                                break;
-                            }
-                        }
-                        i -= 1
-                    }
-                }
-            }
-            _ => {
-                /* In non-strict mode every unquoted value is a primitive */
-                let r = jsmn_parse_primitive(&mut *parser, js, tokens)?;
-                if r < 0 {
-                    return Ok(r);
-                }
-                count += 1;
-                if parser.tok_super != -1 {
-                    tokens[parser.tok_super as usize].size += 1
-                }
-            }
-        }
-        parser.pos += 1;
-    }
-    let mut i = parser.tok_next as i32 - 1;
-    while i >= 0 {
-        /* Unmatched opened object or array */
-        if tokens[i as usize].start != -1 && tokens[i as usize].end == -1 {
-            return Err(JsmnError::Part);
-        }
-        i -= 1
-    }
-    Ok(count)
-}
-/* JSMN_H */
-/* JSMN_HEADER */
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use bcodec::*;
 
 fn main() {
-    let mut p = JsmnParser::new();
-    let mut t = vec![JsmnToken::default(); 128];
-    let s = br#"{"hello": 12}"#;
-    let r = jsmn_parse(&mut p, s, &mut t).unwrap();
+    let mut p = JsonParser::new();
+    let mut t = vec![Token::default(); 128];
+    let s = br#"{"hello": {"hello": {"hello": 12}}}"#;
+    let r = p.parse(s, &mut t).unwrap();
 
     println!("Parsed: {:#?}", &t[..r as usize]);
 }
