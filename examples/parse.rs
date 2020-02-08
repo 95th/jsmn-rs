055c920fbbf6ec6336b75d60eec0224b039634bb
Gurwinder Singh <vargwin@gmail.com>
2020-02-08 07:46:39 +0530

    Update example

--- examples/parse.rs
+++ examples/parse.rs
@@ -3,7 +3,7 @@ use jsmn::*;
 fn main() {
     let s = br#"{"hello": {"hello": {"hello": 12}}}"#;
 
-    let tokens = &mut [Token::default(); 128];
+    let tokens = &mut [Token::default(); 7];
     let p = &mut JsonParser::new();
     let r = p.parse(s, tokens).unwrap();
 
